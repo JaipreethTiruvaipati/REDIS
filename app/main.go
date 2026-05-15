7eaacae39f9a7c639526f94ded9767a5f54d15c3
JaipreethTiruvaipati <jaipreethtsrls17@gmail.com>
2026-05-16 00:21:27 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,13 +6,10 @@ import (
 	"os"
 )
 
-
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment the code below to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
