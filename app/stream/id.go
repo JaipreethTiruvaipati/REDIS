8d5f76461765c58fd2a56cfada00623c6c345352
JaipreethTiruvaipati <jaipreethtsrls17@gmail.com>
2026-05-19 12:02:53 +0530

    codecrafters submit [skip ci]

--- app/stream/id.go
+++ app/stream/id.go
@@ -76,6 +76,10 @@ func GenerateFull(lastID EntryID) EntryID {
 // ParseRangeStart parses a start ID for XRANGE.
 // If no sequence number provided, defaults to 0.
 func ParseRangeStart(idStr string) (EntryID, error) {
+	// "-" means the minimum possible ID (beginning of stream)
+	if idStr == "-" {
+		return EntryID{Milliseconds: 0, Seq: 0}, nil
+	}
 	if !strings.Contains(idStr, "-") {
 		ms, err := strconv.ParseInt(idStr, 10, 64)
 		if err != nil {
@@ -89,10 +93,7 @@ func ParseRangeStart(idStr string) (EntryID, error) {
 // ParseRangeEnd parses an end ID for XRANGE.
 // If no sequence number provided, defaults to the maximum possible sequence.
 func ParseRangeEnd(idStr string) (EntryID, error) {
-	// "-" means the minimum possible ID (beginning of stream)
-	if idStr == "-" {
-		return EntryID{Milliseconds: 0, Seq: 0}, nil
-	}
+
 	if !strings.Contains(idStr, "-") {
 		ms, err := strconv.ParseInt(idStr, 10, 64)
 		if err != nil {
